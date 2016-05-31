a24fd0d53eb3437fd391ba7f0a257c3b4b5ac44d
theRingleman <sam.ringleman@gmail.com>
2016-05-31 17:03:23 -0600

    add controller and erb for new receipt and receipt index page.

--- app/controllers/rcpt_controller.rb
+++ app/controllers/rcpt_controller.rb
@@ -1,3 +1,19 @@
 class RCPTController < ApplicationController
 
+  get "/receipts" do
+    if logged_in?
+      erb :"receipts/index"  
+    else
+      redirect "/login"
+    end
+  end 
+
+  get "/receipts/new" do
+    if logged_in?
+      erb :"/receipts/new"
+    else
+      redirect "/login"
+    end
+  end
+
 end

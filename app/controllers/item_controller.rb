d2fbd7817aa9356f1f3c6a14529e21b9a92ca6f7
theRingleman <sam.ringleman@gmail.com>
2016-06-04 15:50:35 -0600

    add edit functionality for items

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -6,4 +6,13 @@ class ItemController < ApplicationController
     erb :"items/edit"
   end
 
+  post "/item/:id/edit" do
+    redirect_if_not_logged_in
+    @item = Item.find(params[:id])
+    @receipt = @item.receipt
+    @item.update(name: params[:name], price: params[:price])
+    @item.save
+    redirect "/receipts/#{@receipt.id}"
+  end
+
 end
